d8ee56c2787786f396f64493ad8c3193d9a6b2b9
Wamo0 <timilehin.oyadina@gmail.com>
2024-01-02 23:13:58 +0000

    test commit to make sure R-studio and git connected properly

--- R/area.R
+++ R/area.R
@@ -1,3 +1,27 @@
+#' Calculate the area of various 2D and 3D shapes.
+#'
+#' This provides functions to calculate the area of various 2D and 3D shapes,
+#' specifically rectangles, squares, circles, triangles, cuboids, cubes, spheres, and cylinders.
+#' The area can be calculated based on the dimensions of each shape.
+#'
+#' @param shp An object of class 'shape'.
+#' @details The function checks if the input object is of class 'shape'. If not, it raises
+#' an error indicating that the input must be an object of class 'shape'. If the input is
+#' of class 'shape', it dispatches the calculation to the appropriate method based on the
+#' type of shape.
+#'
+#' @family area functions
+#'
+#' @examples
+#' # Create a rectangle object
+#' rectangle <- list(height = 4, width = 6)
+#' class(rectangle) <- "shape"
+#'
+#' # Calculate the area of the rectangle
+#' area(rectangle)
+#'
+#'
+#' @export
 area <- function(shp) {
   if(!inherits(shp,"shape")){
     stop("area can only be passed objects of class shape")
