a9e0dbb3401612e87e7a04b476f27841a4990219
Wamo0 <timilehin.oyadina@gmail.com>
2024-01-05 22:02:23 +0000

    Added documentation for summary and fixed error of example not displaying porperly.

--- R/summary.R
+++ R/summary.R
@@ -1,3 +1,18 @@
+#' Generate a summary for shapes.
+#'
+#' This function provides a summary for objects of class 'shape'. The summary
+#' includes information such as the shape's class, perimeter (for 2D shapes),
+#' surface area (for 3D shapes), and volume (for 3D shapes).
+#'
+#' @param shp An object of class 'shape'.
+#' @details The function checks if the input object is of class 'shape'. If not,
+#' it raises an error indicating that the input must be an object of class 'shape'.
+#' If the input is of class 'shape', it dispatches the summary calculation to the
+#' appropriate method based on the type of shape.
+#'
+#' @family summary functions
+#'
+#' @export
 summary <- function(shp){
   if(!inherits(shp,"shape")){
     stop("This summary function can only be passed objects of class shape")
@@ -7,11 +22,38 @@ summary <- function(shp){
   }
 }
 
+#' Generate a summary of the parameters for 2D shapes.
+#'
+#' @param shp An object of class 'shape' representing a 2D shape.
+#'
+#' @family summary functions
+#'
+#' @examples
+#' my_square <- square(sideLength = 5)
+#'
+#' # Generate a summary for the square
+#' summary.2dShape(my_square)
+#'
+#'
+#' @export
 summary.2dShape <- function(shp){
-  cat("Shape:",class(shp)[3],"\nPerimeter:",perimeter(shp),"\nArea:",area(shp))
+  cat("Shape:", class(shp)[3], "\nPerimeter:", perimeter(shp), "\nArea:", area(shp))
 }
 
+#' Generate a summary of the parameters for 3D shapes.
+#'
+#' @param shp An object of class 'shape' representing a 3D shape.
+#'
+#' @family summary functions
+#'
+#' @examples
+#' # Create a cylinder
+#' my_cylinder <- cylinder(radius = 2, height = 5)
+#'
+#' # Generate a summary for the cylinder
+#' summary.3dShape(my_cylinder)
+#'
+#' @export
 summary.3dShape <- function(shp){
-  cat("Shape:",class(shp)[3],"\nSurface-area:",area(shp),"\nVolume:",volume(shp))
+  cat("Shape:", class(shp)[3], "\nSurface-area:", area(shp), "\nVolume:", volume(shp))
 }
-
