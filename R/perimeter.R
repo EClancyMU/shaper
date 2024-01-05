f3760fb5ca7b7ccc4434d91c324e90e9e3a63841
Wamo0 <timilehin.oyadina@gmail.com>
2024-01-05 22:01:54 +0000

    Added documentation for Perimeter and fixed error of example not displaying porperly.

--- R/perimeter.R
+++ R/perimeter.R
@@ -1,5 +1,24 @@
+
+#' Calculate the perimeter of 2D shapes.
+#'
+#' This package provides functions to calculate the perimeter of various 2D shapes,
+#' including rectangles, squares, circles, and triangles. The perimeter can be
+#' calculated based on the dimensions of each shape.
+#'
+#' @param shp An object of class '2dShape'.
+#' @details The function checks if the input object is of class '2dShape'. If not,
+#' it raises an error indicating that the input must be an object of class '2dShape'.
+#' If the input is of class '2dShape', it dispatches the calculation to the appropriate
+#' method based on the type of shape.
+#'
+#' @family perimeter functions
+#' @seealso \code{\link{perimeter.rectangle}}, \code{\link{perimeter.square}},
+#' \code{\link{perimeter.circle}}, \code{\link{perimeter.triangle}}
+#'
+#'
+#' @export
 perimeter <- function(shp) {
-  if(!inherits(shp,"2dShape")){
+  if(!inherits(shp, "2dShape")){
     stop("perimeter can only be passed objects of class 2dShape")
   }
   else{
@@ -7,18 +26,87 @@ perimeter <- function(shp) {
   }
 }
 
+#' Calculate the perimeter of a rectangle.
+#'
+#' @param rect An object of class '2dShape' representing a rectangle.
+#' @return The perimeter of the rectangle.
+#'
+#' @family perimeter functions
+#' @seealso \code{\link{perimeter}}
+#'
+#' @examples
+#' \dontrun{
+#' # Create a rectangle
+#' my_rectangle <- rectangle(height = 2, width = 4)
+#'
+#' # Calculate the perimeter of the rectangle
+#' perimeter.rectangle(my_rectangle)
+#' }
+#'
+#' @export
 perimeter.rectangle <- function(rect) {
-  return(2*rect$height + 2*rect$width)
+  return(2 * rect$height + 2 * rect$width)
 }
 
+#' Calculate the perimeter of a square.
+#'
+#' @param squ An object of class '2dShape' representing a square.
+#' @return The perimeter of the square.
+#'
+#' @family perimeter functions
+#' @seealso \code{\link{perimeter}}
+#'
+#' @examples
+#' \dontrun{
+#' # Create a square
+#' my_square <- square(sideLength = 5)
+#'
+#' # Calculate the perimeter of the square
+#' perimeter.square(my_square)
+#' }
+#'
+#' @export
 perimeter.square <- function(squ){
   return(4 * squ$sideLength)
 }
 
+#' Calculate the perimeter of a circle.
+#'
+#' @param circ An object of class '2dShape' representing a circle.
+#' @return The perimeter of the circle.
+#'
+#' @family perimeter functions
+#' @seealso \code{\link{perimeter}}
+#'
+#' @examples
+#' # Create a circle
+#' my_circle <- circle(radius = 3)
+#'
+#' # Calculate the perimeter of the circle
+#' perimeter.circle(my_circle)
+#'
+#' @export
 perimeter.circle <- function(circ) {
-  return(2*pi*circ$radius)
+  return(2 * pi * circ$radius)
 }
 
+#' Calculate the perimeter of a triangle.
+#'
+#' @param tri An object of class '2dShape' representing a triangle.
+#' @return The perimeter of the triangle.
+#'
+#' @family perimeter functions
+#' @seealso \code{\link{perimeter}}
+#'
+#' @examples
+#' # Create a triangle
+#' my_triangle <- triangle(a = 3, b = 4, c = 5)
+#'
+#' # Calculate the perimeter of the triangle
+#' perimeter.triangle(my_triangle)
+#'
+#' @export
 perimeter.triangle <- function(tri) {
-  return(tri$a+tri$b+tri$c)
+  return(tri$a + tri$b + tri$c)
 }
+
